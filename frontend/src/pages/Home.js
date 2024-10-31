9f22053c06f760178f2b3d6b3595a7ae431c3bcc
Alyssa <alyssabd12@icloud.com>
2024-10-30 20:48:58 -0400

    frontend home page middle section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,11 +2,24 @@ import './Home.css';
 
 const Home = () => {
     return(
-        <div className="homepage">
-            <h1>R.D. Lights</h1>
-            <p>Custom Pool Lights</p>
-            <p>Handmade Wall Racks</p>
-        </div>
+        <div>
+            <div className="homepage">
+                <h1>R.D. Lights</h1>
+                <p>Custom Pool Lights</p>
+                <p>Handmade Wall Racks</p>
+            </div>
+            <div className="split-section">
+                {/* L section on top of R section to keep ordering accruate */}
+                <div className="Lcontent-section">
+                    <h1>Photo</h1>
+                </div>
+                <div className="Rcontent-section">
+                    <h1>About Me</h1>
+                    <p>My name is Richard</p>
+                </div>
+            </div>
+
+        </div>    
     )
 }
 
